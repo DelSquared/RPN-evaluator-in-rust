7835b6981977c0aa9d0b8ba346c0e627b185e517
DelSquared <37172200+DelSquared@users.noreply.github.com>
2021-12-15 16:57:21 +0100

    Create rpn_eval.rs

---
+++ rpn_eval.rs
@@ -0,0 +1,35 @@
+use std::collections::HashMap;
+
+fn main() {
+
+  let line = "2 2 + 1 -";
+  let ops = "+*-/";
+  let mut bin_num_ops: HashMap<String, fn(i32,i32)->i32> = HashMap::new();
+  bin_num_ops.insert("+".to_string(), |x, y| x + y);
+  bin_num_ops.insert("-".to_string(), |x, y| x - y);
+  bin_num_ops.insert("*".to_string(), |x, y| x * y);
+  bin_num_ops.insert("/".to_string(), |x, y| x / y);
+  let bin_num_ops = bin_num_ops;
+  let mut stack: Vec<i32> = Vec::new();
+  println!("{:?}",stack);
+
+  let line_parse = line.split(' ').collect::<Vec<&str>>();
+
+
+  for t in line_parse.iter(){
+    println!("tkn = {:?}",t);
+    if ops.contains(t){
+      let place_holder = bin_num_ops[&t.to_string()](stack[0], stack[1]);
+      stack = Vec::new();
+      stack.push(place_holder);
+      println!("{:?}",stack);
+
+    } else{
+      stack.push(t.to_string().parse::<i32>().unwrap());
+      println!("{:?}",stack);
+    }
+
+  }
+  println!("result = {:?} (quick maths)",stack);
+
+}
