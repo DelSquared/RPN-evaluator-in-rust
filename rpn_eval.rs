f8f4b97e2d3aa784518b65ca930c5ae4623f4609
DelSquared <37172200+DelSquared@users.noreply.github.com>
2021-12-26 19:28:46 +0100

    implemented stack-wide numerical operations

--- rpn_eval.rs
+++ rpn_eval.rs
@@ -1,9 +1,16 @@
 use std::collections::HashMap;
 
-fn main() {
+fn sum(stack: &Vec<f32>) -> f32{
+  stack.iter().sum()
+}
 
+fn prod(stack: &Vec<f32>) -> f32{
+  stack.iter().product()
+}
+
+fn main() {
   let line = "2 2 + 1 -";
-  let ops = "+*-/%";
+  let ops = "+*-/%$£";
   let mut bin_num_ops: HashMap<String, fn(f32,f32)->f32> = HashMap::new();
   bin_num_ops.insert("+".to_string(), |x, y| x + y);
   bin_num_ops.insert("-".to_string(), |x, y| x - y);
@@ -11,16 +18,25 @@ fn main() {
   bin_num_ops.insert("/".to_string(), |x, y| x / y);
   bin_num_ops.insert("%".to_string(), |x, y| x % y);
   let bin_num_ops = bin_num_ops;
+  let mut stac_num_ops: HashMap<String, fn(&Vec<f32>)->f32> = HashMap::new();
+  stac_num_ops.insert("$".to_string(), |stac| sum(stac));
+  stac_num_ops.insert("£".to_string(), |stac| prod(stac));
+  let stac_num_ops = stac_num_ops;
   let mut stack: Vec<f32> = Vec::new();
   println!("{:?}",stack);
 
   let line_parse = line.split(' ').collect::<Vec<&str>>();
-
+  let mut place_holder: f32;
 
   for t in line_parse.iter(){
+
     println!("tkn = {:?}",t);
     if ops.contains(t){
-      let place_holder = bin_num_ops[&t.to_string()](stack[0], stack[1]);
+      if bin_num_ops.contains_key(&t.to_string()){
+          place_holder = bin_num_ops[&t.to_string()](stack[0], stack[1]);
+      } else {
+          place_holder = stac_num_ops[&t.to_string()](&stack);
+      }
       stack = Vec::new();
       stack.push(place_holder);
       println!("{:?}",stack);
@@ -32,5 +48,4 @@ fn main() {
 
   }
   println!("result = {:?} (quick maths)",stack);
-
 }
